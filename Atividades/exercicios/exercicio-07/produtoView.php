02c0e4f406cbc2b58161189b595ba93377fc0f52
Guilherme Silva Patricio <guigatts@gmail.com>
2021-07-09 19:46:41 -0300

    aula 17

--- Atividades/exercicios/exercicio-07/produtoView.php
+++ Atividades/exercicios/exercicio-07/produtoView.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabela de estados</title>
+    <title>Tabela de produtos</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
