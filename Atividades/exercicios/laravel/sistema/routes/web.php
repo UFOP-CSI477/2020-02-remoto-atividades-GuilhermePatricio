e5728b971502f7f7161431f9bbc37612bd149037
Guilherme Silva Patricio <guigatts@gmail.com>
2021-07-18 17:23:24 -0300

    aula 22

--- Atividades/exercicios/laravel/sistema/routes/web.php
+++ Atividades/exercicios/laravel/sistema/routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos/todos', function(){
+
+    $produtos = Produto::all();
+    return view('lista', ['dados'=>$produtos ]);
+
+});
+
+Route::get('/produtos/{id}', function($id){
+
+    $produto = Produto::findOrFail($id);
+
+    return view('Lista', ['dados'=>$produto]);
+
+});
